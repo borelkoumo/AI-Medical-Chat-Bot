a78fd1568210848d9fccf45f6bd3a8f653825284
Borel KOUMO <borelkoumo@gmail.com>
2025-03-01 17:59:00 +0100

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AI-Medical-ChatBot",
+    version="0.0.1",
+    author="Borel KOUMO",
+    author_email="orelkoumo@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
